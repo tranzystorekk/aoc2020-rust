d902adfb0529a965bc639b8666a7c6e58853cfae
Marcin Puc <marcin.e.puc@gmail.com>
2020-11-30 12:16:34 +0100

    Add day 1 solution template

--- src/days/d01p1.rs
+++ src/days/d01p1.rs
@@ -1,3 +1,13 @@
+use aoc_utils::BufferedInput;
+
+fn parse_input() -> std::io::Result<()> {
+    let input = BufferedInput::parse_args("Day 1: TODO - Part 1")?;
+
+    todo!()
+}
+
 fn main() -> std::io::Result<()> {
+    let input = parse_input()?;
+
     Ok(())
 }
