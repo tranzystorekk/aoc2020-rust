c2f7ac0375f23e123b9d764971a74f0242879619
Marcin Puc <marcin.e.puc@gmail.com>
2020-12-01 06:57:06 +0100

    Add solutions for day 1

--- src/days/d01p1.rs
+++ src/days/d01p1.rs
@@ -1,13 +1,35 @@
 use aoc_utils::BufferedInput;
+use itertools::Itertools;
+use std::{collections::HashSet, io::BufRead};
 
-fn parse_input() -> std::io::Result<()> {
-    let input = BufferedInput::parse_args("Day 1: TODO - Part 1")?;
+fn parse_input() -> std::io::Result<Vec<i32>> {
+    let input = BufferedInput::parse_args("Day 1: Report Repair - Part 1")?;
 
-    todo!()
+    input
+        .lines()
+        .map_results(|line| line.parse().unwrap())
+        .collect()
 }
 
 fn main() -> std::io::Result<()> {
-    let input = parse_input()?;
+    let records = parse_input()?;
+    let mut remainders = HashSet::new();
+
+    let second = records
+        .into_iter()
+        .find(|&entry| {
+            if remainders.contains(&entry) {
+                return true;
+            }
+
+            let other = 2020 - entry;
+            !remainders.insert(other)
+        })
+        .unwrap();
+    
+    let first = 2020 - second;
+
+    println!("{}", first * second);
 
     Ok(())
 }
---
+++ src/days/d01p2.rs
@@ -0,0 +1,43 @@
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+use std::{collections::HashSet, io::BufRead};
+
+fn parse_input() -> std::io::Result<Vec<i32>> {
+    let input = BufferedInput::parse_args("Day 1: Report Repair - Part 2")?;
+
+    input
+        .lines()
+        .map_results(|line| line.parse().unwrap())
+        .collect()
+}
+
+fn try_sum_two(first: i32, records: &[i32]) -> Option<(i32, i32, i32)> {
+    let mut remainders = HashSet::new();
+    let remaining_sum = 2020 - first;
+
+    records
+        .iter()
+        .filter(|&&el| el != first)
+        .find(|&&record| {
+            if remainders.contains(&record) {
+                return true;
+            }
+
+            let other = remaining_sum - record;
+            !remainders.insert(other)
+        })
+        .map(|&n| (first, remaining_sum - n, n))
+}
+
+fn main() -> std::io::Result<()> {
+    let records = parse_input()?;
+
+    let (first, second, third) = records
+        .iter()
+        .find_map(|&first| try_sum_two(first, &records))
+        .unwrap();
+
+    println!("{}", first * second * third);
+
+    Ok(())
+}
