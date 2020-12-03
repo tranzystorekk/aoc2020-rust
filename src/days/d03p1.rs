d79b62830ebb90cd24d8e102e415ffbae3cb5112
Marcin Puc <marcin.e.puc@gmail.com>
2020-12-03 06:39:38 +0100

    Add solutions for day 3

---
+++ src/days/d03p1.rs
@@ -0,0 +1,37 @@
+use std::io::BufRead;
+
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+
+fn parse_input() -> std::io::Result<Vec<Vec<bool>>> {
+    let input = BufferedInput::parse_args("Day 3: Toboggan Trajectory - Part 1")?;
+
+    input
+        .lines()
+        .map_results(|line| line.chars().map(|c| c == '#').collect())
+        .collect()
+}
+
+fn get_slope(w: usize, h: usize) -> impl Iterator<Item = (usize, usize)> {
+    itertools::iterate((0, 0), move |&(x, y)| (x + w, y + h))
+}
+
+fn main() -> std::io::Result<()> {
+    let land_map = parse_input()?;
+    let vertical_dist = land_map.len();
+    let horizontal_dist = land_map[0].len();
+
+    let n_trees = get_slope(3, 1)
+        .take_while(|&(_, y)| y < vertical_dist)
+        .filter(|&(x, y)| {
+            let row = &land_map[y];
+            let mapped_x = x % horizontal_dist;
+
+            row[mapped_x]
+        })
+        .count();
+
+    println!("{}", n_trees);
+
+    Ok(())
+}
---
+++ src/days/d03p2.rs
@@ -0,0 +1,52 @@
+use std::io::BufRead;
+
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+
+fn parse_input() -> std::io::Result<Vec<Vec<bool>>> {
+    let input = BufferedInput::parse_args("Day 3: Toboggan Trajectory - Part 2")?;
+
+    input
+        .lines()
+        .map_results(|line| line.chars().map(|c| c == '#').collect())
+        .collect()
+}
+
+fn get_slope(w: usize, h: usize) -> impl Iterator<Item = (usize, usize)> {
+    itertools::iterate((0, 0), move |&(x, y)| (x + w, y + h))
+}
+
+struct LandMap {
+    map: Vec<Vec<bool>>,
+}
+
+impl LandMap {
+    fn count_slope_trees(&self, w: usize, h: usize) -> usize {
+        let vertical_dist = self.map.len();
+        let horizontal_dist = self.map[0].len();
+
+        get_slope(w, h)
+            .take_while(|&(_, y)| y < vertical_dist)
+            .filter(|&(x, y)| {
+                let row = &self.map[y];
+                let mapped_x = x % horizontal_dist;
+
+                row[mapped_x]
+            })
+            .count()
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let raw_map = parse_input()?;
+    let land = LandMap { map: raw_map };
+
+    let result: usize = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|&(w, h)| land.count_slope_trees(w, h))
+        .product();
+
+    println!("{}", result);
+
+    Ok(())
+}
