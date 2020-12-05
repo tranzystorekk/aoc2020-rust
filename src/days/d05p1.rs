e2e17840118afaa0a64cfddf1072aa2f1c87343c
Marcin Puc <marcin.e.puc@gmail.com>
2020-12-05 09:03:09 +0100

    Simplify id processing in day 5

--- src/days/d05p1.rs
+++ src/days/d05p1.rs
@@ -1,48 +1,26 @@
 use std::io::BufRead;
 
 use aoc_utils::BufferedInput;
-use itertools::Itertools;
 
-fn parse_input() -> std::io::Result<Vec<BoardingPass>> {
+fn parse_input() -> std::io::Result<Vec<String>> {
     let input = BufferedInput::parse_args("Day 5: Binary Boarding - Part 1")?;
 
-    input
-        .lines()
-        .map_results(|line| BoardingPass {
-            row: line[..7].into(),
-            column: line[7..].into(),
+    input.lines().collect()
+}
+
+fn to_id(pass: &str) -> i32 {
+    itertools::zip((0..10).rev(), pass.chars())
+        .map(|(n, c)| {
+            let bit = ['B', 'R'].contains(&c) as i32;
+            bit << n
         })
-        .collect()
-}
-
-struct BoardingPass {
-    row: String,
-    column: String,
-}
-
-impl BoardingPass {
-    pub fn id(&self) -> i32 {
-        let r: i32 = itertools::zip((0..7).rev(), self.row.chars())
-            .map(|(n, c)| {
-                let bit = (c == 'B') as i32;
-                bit << n
-            })
-            .sum();
-        let c: i32 = itertools::zip((0..3).rev(), self.column.chars())
-            .map(|(n, c)| {
-                let bit = (c == 'R') as i32;
-                bit << n
-            })
-            .sum();
-
-        r * 8 + c
-    }
+        .sum()
 }
 
 fn main() -> std::io::Result<()> {
     let passes = parse_input()?;
 
-    let max_id = passes.iter().map(BoardingPass::id).max().unwrap();
+    let max_id = passes.iter().map(|pass| to_id(pass)).max().unwrap();
 
     println!("{}", max_id);
 
