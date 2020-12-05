a086bd314cd73e4cba4f633a26ab60a476e1100d
Marcin Puc <marcin.e.puc@gmail.com>
2020-12-05 07:11:03 +0100

    Add solutions for day 5

---
+++ src/days/d05p2.rs
@@ -0,0 +1,56 @@
+use std::io::BufRead;
+
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+
+fn parse_input() -> std::io::Result<Vec<BoardingPass>> {
+    let input = BufferedInput::parse_args("Day 5: Binary Boarding - Part 2")?;
+
+    input
+        .lines()
+        .map_results(|line| BoardingPass {
+            row: line[..7].into(),
+            column: line[7..].into(),
+        })
+        .collect()
+}
+
+struct BoardingPass {
+    row: String,
+    column: String,
+}
+
+impl BoardingPass {
+    pub fn id(&self) -> i32 {
+        let r: i32 = itertools::zip((0..7).rev(), self.row.chars())
+            .map(|(n, c)| {
+                let bit = (c == 'B') as i32;
+                bit << n
+            })
+            .sum();
+        let c: i32 = itertools::zip((0..3).rev(), self.column.chars())
+            .map(|(n, c)| {
+                let bit = (c == 'R') as i32;
+                bit << n
+            })
+            .sum();
+
+        r * 8 + c
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let passes = parse_input()?;
+
+    let missing_id = passes
+        .iter()
+        .map(BoardingPass::id)
+        .sorted()
+        .tuple_windows()
+        .find_map(|(a, b)| Some(a + 1).filter(|_| b != a + 1))
+        .unwrap();
+
+    println!("{}", missing_id);
+
+    Ok(())
+}
