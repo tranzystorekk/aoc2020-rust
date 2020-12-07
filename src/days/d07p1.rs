49e588dd42f800603c3cc1cab7c000754f042042
Marcin Puc <marcin.e.puc@gmail.com>
2020-12-07 07:56:28 +0100

    Add solutions for day 7

---
+++ src/days/d07p1.rs
@@ -0,0 +1,65 @@
+use std::{collections::HashMap, io::BufRead};
+
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+
+type Regulations = HashMap<String, Vec<String>>;
+
+fn parse_input() -> std::io::Result<Regulations> {
+    let input = BufferedInput::parse_args("Day 7: Handy Haversacks - Part 1")?;
+
+    input
+        .lines()
+        .map_results(|line| {
+            let (outer, contents) = line.split(" contain ").collect_tuple().unwrap();
+            let outer = outer.split_whitespace().take(2).join(" ");
+
+            let contents = contents
+                .split(", ")
+                .filter_map(|descr| {
+                    let (n, ca, cb) = descr.split_whitespace().next_tuple().unwrap();
+
+                    if n == "no" {
+                        return None;
+                    }
+
+                    let result_color = format!("{} {}", ca, cb);
+                    Some(result_color)
+                })
+                .collect();
+
+            (outer, contents)
+        })
+        .collect()
+}
+
+fn can_contain_shiny_gold(regulations: &Regulations, outer: &str) -> bool {
+    let mut searchspace = vec![outer];
+
+    while let Some(container) = searchspace.pop() {
+        if container == "shiny gold" {
+            return true;
+        }
+
+        let inner = &regulations[container];
+        let search_candidates = inner.iter().map(String::as_str);
+
+        searchspace.extend(search_candidates);
+    }
+
+    false
+}
+
+fn main() -> std::io::Result<()> {
+    let regulations = parse_input()?;
+
+    let n_accepted_containers = regulations
+        .keys()
+        .filter(|&c| c != "shiny gold")
+        .filter(|c| can_contain_shiny_gold(&regulations, c))
+        .count();
+
+    println!("{}", n_accepted_containers);
+
+    Ok(())
+}
---
+++ src/days/d07p2.rs
@@ -0,0 +1,66 @@
+use std::{collections::HashMap, io::BufRead};
+
+use aoc_utils::BufferedInput;
+use itertools::Itertools;
+
+type Regulations = HashMap<String, Vec<(String, i32)>>;
+
+fn parse_input() -> std::io::Result<Regulations> {
+    let input = BufferedInput::parse_args("Day 7: Handy Haversacks - Part 2")?;
+
+    input
+        .lines()
+        .map_results(|line| {
+            let (outer, contents) = line.split(" contain ").collect_tuple().unwrap();
+            let outer = outer.split_whitespace().take(2).join(" ");
+
+            let contents = contents
+                .split(", ")
+                .filter_map(|descr| {
+                    let (n, ca, cb) = descr.split_whitespace().next_tuple().unwrap();
+
+                    if n == "no" {
+                        return None;
+                    }
+
+                    let result_color = format!("{} {}", ca, cb);
+                    let number = n.parse().unwrap();
+                    Some((result_color, number))
+                })
+                .collect();
+
+            (outer, contents)
+        })
+        .collect()
+}
+
+fn count_inner_bags(regulations: &Regulations, outer: &str) -> i32 {
+    let mut searchspace: Vec<_> = regulations[outer]
+        .iter()
+        .map(|(c, n)| (c.as_str(), *n))
+        .collect();
+    let mut result = 0;
+
+    while let Some((container, n)) = searchspace.pop() {
+        result += n;
+
+        let inner = &regulations[container];
+        let search_candidates = inner
+            .iter()
+            .map(|(color, num_regulated)| (color.as_str(), n * num_regulated));
+
+        searchspace.extend(search_candidates);
+    }
+
+    result
+}
+
+fn main() -> std::io::Result<()> {
+    let regulations = parse_input()?;
+
+    let n_needed_bags = count_inner_bags(&regulations, "shiny gold");
+
+    println!("{}", n_needed_bags);
+
+    Ok(())
+}
