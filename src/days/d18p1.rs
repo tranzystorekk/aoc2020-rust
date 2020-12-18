b62c4612a8068078391e468d63c68b195d773bc0
Marcin Puc <marcin.e.puc@gmail.com>
2020-12-18 11:22:52 +0100

    Add solutions for day 18

---
+++ src/days/d18p1.rs
@@ -0,0 +1,105 @@
+use std::io::BufRead;
+
+use aoc_utils::BufferedInput;
+
+fn parse_input() -> std::io::Result<Vec<String>> {
+    let input = BufferedInput::parse_args("Day 18: Operation Order - Part 1")?;
+
+    input.lines().collect()
+}
+
+#[derive(Clone, Copy)]
+enum Op {
+    Add,
+    Mul,
+    Val(u64),
+}
+
+#[derive(Clone, Copy)]
+enum Token {
+    Op(char),
+    LeftParen,
+}
+
+impl Token {
+    pub fn into_op(self) -> Op {
+        match self {
+            Token::Op('+') => Op::Add,
+            Token::Op('*') => Op::Mul,
+            _ => panic!("Tried to convert inappropriate token"),
+        }
+    }
+}
+
+fn parse_infix(s: &str) -> Vec<Op> {
+    let mut result = Vec::new();
+    let mut ops = Vec::new();
+
+    let atoms = s.split_whitespace().flat_map(str::chars);
+    for atom in atoms {
+        match atom {
+            op @ '+' | op @ '*' => {
+                while let Some(t @ Token::Op(_)) = ops.last() {
+                    result.push(t.into_op());
+                    ops.pop();
+                }
+                ops.push(Token::Op(op));
+            }
+            '(' => ops.push(Token::LeftParen),
+            ')' => {
+                while let Some(t @ Token::Op(_)) = ops.pop() {
+                    result.push(t.into_op());
+                }
+            }
+            n => {
+                let v = n.to_digit(10).unwrap();
+
+                result.push(Op::Val(v as u64));
+            }
+        }
+    }
+
+    let rest = ops.into_iter().rev().map(Token::into_op);
+    result.extend(rest);
+
+    result
+}
+
+fn eval(rpn: Vec<Op>) -> u64 {
+    let mut partials = Vec::new();
+
+    for el in rpn {
+        match el {
+            Op::Val(n) => partials.push(n),
+            Op::Add => {
+                let (a, b) = (partials.pop().unwrap(), partials.pop().unwrap());
+                partials.push(a + b);
+            }
+            Op::Mul => {
+                let (a, b) = (partials.pop().unwrap(), partials.pop().unwrap());
+                partials.push(a * b);
+            }
+        }
+    }
+
+    partials.pop().unwrap()
+}
+
+fn main() -> std::io::Result<()> {
+    let exprs = parse_input()?;
+
+    let (elapsed, result): (_, u64) = elapsed::measure_time(|| {
+        exprs
+            .into_iter()
+            .map(|expr| {
+                let rpn = parse_infix(&expr);
+                eval(rpn)
+            })
+            .sum()
+    });
+
+    eprintln!("{}", elapsed);
+    println!("{}", result);
+
+    Ok(())
+}
